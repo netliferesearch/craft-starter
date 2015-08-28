46dc77ce67e2578e7b701aa26df41af11a64d089
Raymond Julin <raymond.julin@gmail.com>
2015-08-28 15:21:04 +0200

    Support serving static files from PHP web server properly

--- public/index.php
+++ public/index.php
@@ -1,4 +1,13 @@
 <?php
+define('CRAFT_LOCALE', 'nb');
+
+if (php_sapi_name() === 'cli-server') {
+    $file = realpath(getcwd() . $_SERVER['REQUEST_URI']);
+
+    if ($file && is_file($file)) {
+        return false;
+    }
+}
 
 // Path to your craft/ folder
 $craftPath = '../craft';
@@ -6,10 +15,8 @@ $craftPath = '../craft';
 // Do not edit below this line
 $path = rtrim($craftPath, '/').'/app/index.php';
 
-if (!is_file($path))
-{
-	if (function_exists('http_response_code'))
-	{
+if (!is_file($path)) {
+	if (function_exists('http_response_code')) {
 		http_response_code(503);
 	}
 
