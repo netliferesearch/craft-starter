2f835c39aba8024e13dc9dec81e5b06732f78723
Raymond Julin <raymond.julin@gmail.com>
2015-12-11 14:37:11 +0100

    Only allow craft updates on localhost

--- craft/config/general.php
+++ craft/config/general.php
@@ -5,12 +5,14 @@ return array(
     '*' => array(
         'generateTransformsBeforePageLoad' => true,
         'defaultCpLanguage' => 'en',
+        'allowAutoUpdates' => false,
         'cache' => true,
     ),
     'localhost' => array(
         'devMode' => true,
         'cache' => false,
         'siteUrl' => 'http://localhost:5000/',
+        'allowAutoUpdates' => true,
         'environmentVariables' => array(
             'siteUrl' => 'http://localhost:5000',
             'basePath' => realpath(getcwd() . '/public/')
@@ -20,6 +22,7 @@ return array(
         'omitScriptNameInUrls' => true,
         'siteUrl' => 'http://myapp.herokuapp.com/',
         'postLoginRedirect' => '/',
+        'allowAutoUpdates' => false,
         'environmentVariables' => array(
             'siteUrl' => 'http://myapp.herokuapp.com',
             'basePath' => realpath(getcwd() . '/public/')
