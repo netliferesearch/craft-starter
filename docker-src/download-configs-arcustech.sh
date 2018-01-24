bc5f6c53ae6f5489d3bbc5f594f8e7a5272ad039
Nils Norman Haukås <nils@thunki.com>
2018-01-24 21:15:39 +0100

    remove unused file

--- docker-src/download-configs-arcustech.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Downloading remote config files"
-
-rsync -avz av04900@185.193.216.36:/storage/av04900/www/public_html/craft/config/db.php craft/config/db.php
-rsync -avz av04900@185.193.216.36:/storage/av04900/www/public_html/craft/config/general.php craft/config/general.php
