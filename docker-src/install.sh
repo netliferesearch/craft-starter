84621b72ba5cc95a10086828790740a8bbeb465e
Nils Norman Haukås <nils@thunki.com>
2018-01-30 16:08:28 +0100

    use externally built docker image as base for server service

--- docker-src/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This is a startup script that gets added by the Dockerfile
-# so that composer dependencies will be installed on startup.
-composer install -d /var/www/html
-
-apache2-foreground
