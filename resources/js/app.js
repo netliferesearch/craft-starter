fd66530abfe96ef363819dd7e328d0d6dbe93491
Nils Norman Haukås <nils@thunki.com>
2018-01-31 14:26:39 +0100

    start including webpack. Clean up public/ folder

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+const helloES6World = () => {
+  // Quick test to see if we can build and include JS.
+  // This will throw in IE8 if we have not polyfilled
+  // Array.from().
+  const array = Array.from('hello world')
+  console.log(array.join(''))
+}
