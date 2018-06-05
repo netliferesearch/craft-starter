630f43c474235d8f96dbf79bb23c7ce17d21a886
Nils Norman Haukås <nils@thunki.com>
2018-06-05 19:06:17 +0200

    add webpack, with sample files and postcss config

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,21 @@
 const path = require('path')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 
 module.exports = {
-  context: path.resolve(__dirname),
-  entry: [
-    './resources/js/main.js',
-    './resources/css/main.css'
-  ],
+  entry: ['./web-src/app.js', './web-src/style.css'],
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public/webpack-dist')
+    path: path.resolve(__dirname, 'web')
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [{ loader: 'css-loader' }, { loader: 'postcss-loader' }]
-        })
-      },
-      {
-        test: /\.(eot|gif|woff|woff2|png|ttf)([?]?.*)$/,
-        use: ['file-loader?name=[name].[ext]&publicPath=/webpack-dist/']
-      },
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader'
+        ]
       }
     ]
-  },
-  plugins: [
-    new ExtractTextPlugin('screen.css'),
-    new BrowserSyncPlugin({
-      host: 'localhost',
-      port: 3000,
-      cors: true,
-      notify: false,
-      proxy: 'http://localhost:5000',
-      files: ['./craft/config', './craft/templates']
-    })
-  ]
+  }
 }
