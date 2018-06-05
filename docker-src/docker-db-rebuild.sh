02f02ee86c226f70a028aa246084dbd744dfb05c
Nils Norman Haukås <nils@thunki.com>
2018-06-05 10:19:22 +0200

    replace craft2 with vanilla craft3 built with composer

--- docker-src/docker-db-rebuild.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Use when you want to replace (refresh) the database while docker-compose is
-# already running.
-docker-compose rm -f -s -v database && \
-  docker-compose up -d database
