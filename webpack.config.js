aa892b7f8f4cd5867d8ed4da212fcd1ea400addd
Ådne Forslund <adne.forslund@gmail.com>
2019-08-06 12:42:47 +0200

    change web to public to make it run on arcus

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-const path = require('path')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 
 module.exports = {
   context: path.resolve(__dirname),
-  entry: ['./web/js/main.js', './web/css/main.css'],
+  entry: ["./public/js/main.js", "./public/css/main.css"],
   output: {
-    filename: '[name].dist.js',
-    path: path.resolve(__dirname, 'web/dist')
+    filename: "[name].dist.js",
+    path: path.resolve(__dirname, "public/dist")
   },
   module: {
     rules: [
@@ -15,38 +15,38 @@ module.exports = {
         test: /\.css$/,
         use: [
           MiniCssExtractPlugin.loader,
-          { loader: 'css-loader' },
-          { loader: 'postcss-loader' }
+          { loader: "css-loader" },
+          { loader: "postcss-loader" }
         ]
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['babel-preset-env']
+            presets: ["babel-preset-env"]
           }
         }
       },
       {
-        // if a JS file requires a file it will be moved to ./web/dist
+        // if a JS file requires a file it will be moved to ./public/dist
         test: /\.(eot|gif|woff|woff2|png|ttf)([?]?.*)$/,
-        use: ['file-loader?name=[name].[ext]&publicPath=/dist/']
+        use: ["file-loader?name=[name].[ext]&publicPath=/dist/"]
       }
     ]
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: '[name].dist.css'
+      filename: "[name].dist.css"
     }),
     new BrowserSyncPlugin({
-      host: 'localhost',
+      host: "localhost",
       port: 3000,
       cors: true,
       notify: false,
-      proxy: 'http://localhost:5000',
-      files: ['./config', './templates']
+      proxy: "http://localhost:5000",
+      files: ["./config", "./templates"]
     })
   ]
-}
+};
