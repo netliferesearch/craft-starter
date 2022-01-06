57cd992971f2a7cc7be965743d4aba6de635540a
Svale Fossåskaret <svaleaf@gmail.com>
2022-01-06 15:19:53 +0100

    Remove webpack and add vite
    Including dependecies

---
+++ vite.config.js
@@ -0,0 +1,53 @@
+import legacy from '@vitejs/plugin-legacy'
+import ViteRestart from 'vite-plugin-restart'
+import mkcert from 'vite-plugin-mkcert'
+import copy from 'rollup-plugin-copy'
+
+// https://vitejs.dev/config/
+export default ({ command }) => ({
+  base: command === 'serve' ? '' : '/dist/',
+  build: {
+    emptyOutDir: true,
+    manifest: true,
+    outDir: './public/dist/',
+    rollupOptions: {
+      input: {
+        app: './resources/js/main.js',
+      },
+    },
+  },
+  plugins: [
+    // This plugin copies files that should be included in the public (dist) folder on build
+    copy({
+      targets: [
+        // { src: 'resources/images/**/*', dest: 'web/images' },
+        // { src: 'resources/fonts/.htaccess', dest: 'web/dist/assets' },
+      ],
+      hook: 'writeBundle',
+    }),
+
+    // This plugin provides support for legacy browsers that do not support native ESM.
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+
+    // Plugin that add change listeners to restart the server
+    ViteRestart({
+      reload: ['./templates/**/*'],
+    }),
+
+    // Plugin that provides certificate support for vite https development services.
+    mkcert(),
+  ],
+
+  // publicDir: '../resources/public',
+
+  server: {
+    origin: 'https://localhost:3000',
+    // host: '0.0.0.0',
+    https: true,
+    hmr: {
+      host: 'localhost',
+    },
+  },
+})
